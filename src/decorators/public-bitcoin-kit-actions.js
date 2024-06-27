8eba53f71df3525939d2500aa58434f1bf375f44
Gabriel Montes <gabriel@bloq.com>
2024-06-27 17:48:34 -0400

    Simplify decorators logic

--- src/decorators/public-bitcoin-kit-actions.js
+++ src/decorators/public-bitcoin-kit-actions.js
@@ -1,25 +1,6 @@
-import {
-  getBitcoinAddressBalance,
-  getHeaderN,
-  getLastHeader,
-  getTransactionByTxId,
-  getTxConfirmations,
-  getUtxosForBitcoinAddress,
-} from "../actions/bitcoin-kit.js"
+import { actionsToDecorator } from "./actions-to-decorator.js"
+import * as bitcoinKitActions from "../actions/bitcoin-kit.js"
 
 export const hemiPublicBitcoinKitActions = function () {
-  return function (client) {
-    return {
-      getBitcoinAddressBalance: (parameters) =>
-        getBitcoinAddressBalance(client, parameters),
-      getHeaderN: (parameters) => getHeaderN(client, parameters),
-      getLastHeader: () => getLastHeader(client),
-      getTransactionByTxId: (parameters) =>
-        getTransactionByTxId(client, parameters),
-      getTxConfirmations: (parameters) =>
-        getTxConfirmations(client, parameters),
-      getUtxosForBitcoinAddress: (parameters) =>
-        getUtxosForBitcoinAddress(client, parameters),
-    }
-  }
+  return actionsToDecorator(bitcoinKitActions)
 }
