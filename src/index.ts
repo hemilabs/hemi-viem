6e44bd8ab45b6c870185309655edbd5365cedf11
Gonzalo D'Elia <gonzalo@bloq.com>
2024-07-12 10:41:37 -0300

    Export everything from index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 export { hemi } from "./chains/hemi.js"
 export { hemiSepolia } from "./chains/hemi-sepolia.js"
 
-export { getBtcFinalityByBlockHash } from "./actions/get-btc-finality-by-block-hash.js"
+export * from "./actions/bitcoin-kit.js"
+export * from "./actions/bitcoin-tunnel-manager.js"
+export * from "./actions/bitcoin-vault.js"
+export * from "./actions/get-btc-finality-by-block-hash.js"
 
 export { hemiPublicBitcoinKitActions } from "./decorators/public-bitcoin-kit-actions.js"
 export { hemiPublicBitcoinTunnelManagerActions } from "./decorators/public-bitcoin-tunnel-manager-actions.js"
 export { hemiPublicBitcoinVaultActions } from "./decorators/public-bitcoin-vault-actions.js"
 export { hemiPublicOpNodeActions } from "./decorators/public-op-node-actions.js"
-
-export {
-  getBitcoinAddressBalance,
-  getHeaderN,
-  getLastHeader,
-  getTransactionByTxId,
-  getTxConfirmations,
-  getUtxosForBitcoinAddress,
-} from "./actions/bitcoin-kit.js"
