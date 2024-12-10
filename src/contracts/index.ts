425abfa572f5244b51fedd908176b2de3a58c3ff
Gonzalo D'Elia <gonzalo@bloq.com>
2024-12-10 17:54:37 -0300

    Use barrel file for exporting abis

---
+++ src/contracts/index.ts
@@ -0,0 +1,4 @@
+export { bitcoinKitTxsAbi } from "./bitcoin-kit-txs";
+export { bitcoinTunnelManagerAbi } from "./bitcoin-tunnel-manager";
+export { simpleBitcoinVaultStateAbi } from "./simple-bitcoin-vault-state";
+export { simpleBitcoinVaultAbi } from "./simple-bitcoin-vault";
