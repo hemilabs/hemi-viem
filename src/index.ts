3afc895825f1cab8b824d1fbbc912c0b15be48b9
Artur Dolzan Neto <36503078+ArturDolzan@users.noreply.github.com>
2025-05-15 12:23:19 -0300

    Add Bitcoin Tunnel encoders (#48)

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ export { hemi } from "./chains/hemi.js";
 export { hemiSepolia } from "./chains/hemi-sepolia.js";
 
 export * from "./actions/public/bitcoin-kit.js";
+export {
+  encodeChallengeWithdrawal,
+  encodeConfirmDeposit,
+  encodeInitiateWithdrawal,
+} from "./actions/public/bitcoin-tunnel-encoders.js";
 export * from "./actions/public/bitcoin-tunnel-manager.js";
 export * from "./actions/public/simple-bitcoin-vault.js";
 export * from "./actions/public/simple-bitcoin-vault-state.js";
 export * from "./actions/public/get-btc-finality-by-block-hash.js";
+export { bitcoinTunnelManagerAddresses } from "./contracts/bitcoin-tunnel-manager.js";
 
 export { hemiPublicBitcoinKitActions } from "./decorators/public-bitcoin-kit-actions.js";
 export { hemiPublicBitcoinTunnelManagerActions } from "./decorators/public-bitcoin-tunnel-manager-actions.js";
