254b6026a30bb0642e609eabaf89f8a6f6dee36c
Gabriel Montes <gabriel@hemi.xyz>
2025-06-17 15:33:21 -0400

    Add many actions for the vault monitor

---
+++ src/contracts/global-config.ts
@@ -0,0 +1,15 @@
+export const globalConfigAbi = [
+  {
+    inputs: [],
+    name: "withdrawalsPaused",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
