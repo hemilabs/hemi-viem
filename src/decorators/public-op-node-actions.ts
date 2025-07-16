661900f15f0dfa2e29d5f19f81e8a871f9de95f9
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-07-15 21:15:51 -0300

    Fix eslint warnings

--- src/decorators/public-op-node-actions.ts
+++ src/decorators/public-op-node-actions.ts
@@ -1,4 +1,5 @@
-import { actionsToDecorator } from "./actions-to-decorator.js";
 import * as opNodeActions from "../actions/public/get-btc-finality-by-block-hash.js";
+
+import { actionsToDecorator } from "./actions-to-decorator.js";
 
 export const hemiPublicOpNodeActions = () => actionsToDecorator(opNodeActions);
